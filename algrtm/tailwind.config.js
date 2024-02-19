2a1f20f96f1f8c3305a29c6e7ff3916a7f17a593
Fermatka <agatka.fermatka@gmail.com>
2024-02-19 09:46:53 +0100

    global styles, colors, font and typography added to project and default styles and settings reseted

--- algrtm/tailwind.config.js
+++ algrtm/tailwind.config.js
@@ -6,11 +6,36 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blueLight: {
+        blue20: "#1B264F",
+        blue40: "#274690",
+        blue50: "#576CA8",
+      },
+      blueDark: {
+        blue20: "#576CA8",
+        blue40: "#274690",
+        blue50: "#1B264F",
+      },
+      neutralLight: {
+        neutral10: "#1C1C1C",
+        neutral20: "#2E2E2E",
+        neutral40: "#808080",
+        neutral60: "#B3B3B3",
+        neutral80: "#D6D6D6",
+        neutral90: "#F5F3F5",
+        neutral100: "#FFFFFF",
+      },
+      neutralDark: {
+        neutral10: "#FFFFFF",
+        neutral20: "#F5F3F5",
+        neutral40: "#D6D6D6",
+        neutral60: "#B3B3B3",
+        neutral80: "#808080",
+        neutral90: "#2E2E2E",
+        neutral100: "#1C1C1C",
       },
     },
   },

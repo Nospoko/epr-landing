581aa9eed79ecfd938ec0c8d18ae7be29f61ba7f
Fermatka <agatka.fermatka@gmail.com>
2024-02-19 14:15:19 +0100

    routing done

--- algrtm/src/app/components/Header.js
+++ algrtm/src/app/components/Header.js
@@ -10,24 +10,38 @@ export default function Header() {
     <header className="z-[999] relative flex w-full justify-between items-center px-6 py-1">
       <div className="flex w-full flex-wrap items-center justify-center gap-y-1 p3SB text-neutralLight-neutral10 sm:w-[initial] sm:flex-nowrap sm:gap-5">
         <div className="flex w-full items-center justify-center px-3 py-3 hover:text-blue transition">
-          <p className="text-blueLight-blue50 h5 ">A</p>
-          <p className="text-neutralLight-neutral10 h5">lgrtm</p>
+          <Link href="/">
+            <p className="text-neutralLight-neutral10 h5">Algrtm</p>
+          </Link>
         </div>
       </div>
 
       <div>
         <div className="flex w-full flex-wrap items-center justify-center gap-y-1 text-neutralLight-neutral10 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-          {links.map((link) => (
-            <div className="flex items-center justify-center" key={link.path}>
-              <Link
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-blue transition p3 "
-                href={link.path}
-              >
-                {link.name}
-              </Link>
-            </div>
-          ))}
-          <button className="buttonPrimary p3">Contact Us</button>
+          <div className="flex items-center justify-center">
+            <Link
+              className="flex w-full items-center justify-center px-3 py-3 hover:text-blue transition p3 "
+              href="/pianoRoll"
+            >
+              PianoRoll
+            </Link>
+            <Link
+              className="flex w-full items-center justify-center px-3 py-3 hover:text-blue transition p3 "
+              href="/customProjects"
+            >
+              CustomProjects
+            </Link>
+            <Link
+              className="flex w-full items-center justify-center px-3 py-3 hover:text-blue transition p3 "
+              href="/knowledge"
+            >
+              Knowledge
+            </Link>
+          </div>
+
+          <Link href="/contactUs">
+            <button className="buttonPrimary p3">Contact Us</button>
+          </Link>
           <button className="buttonSecondary">
             <div className="flex items-center justify-center">
               <SunIcon />
---
+++ algrtm/src/app/customProjects/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomProjects = () => {
+  return <div>CustomProjects</div>;
+};
+
+export default CustomProjects;

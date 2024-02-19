8b8f9041f03ff16f88d2fa3351d8b2235e45db67
Tomasz Roszczynialski <roszcz@gmail.com>
2024-02-19 19:02:18 +0100

    update config

--- algrtm/next.config.js
+++ algrtm/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  basePath: '/algrtm',
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,

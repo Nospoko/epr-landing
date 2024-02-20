3d7f627b08aeb26992ae31dfa6671bbfabda2250
Fermatka <agatka.fermatka@gmail.com>
2024-02-20 11:01:01 +0100

    styles refactor and routes update

--- algrtm/src/app/components/Header.js
+++ algrtm/src/app/components/Header.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { links } from "../lib/data";
 import Link from "next/link";
-
 import SunIcon from "../assets/icons/SunIcon";
 import Image from "next/image";
 
@@ -9,11 +7,12 @@ export default function Header() {
   return (
     <header className="z-[999] relative flex w-full justify-between items-center px-6 py-1">
       <div className="flex w-full flex-wrap items-center justify-center gap-y-1 p3SB text-neutralLight-neutral10 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-        <div className="flex w-full items-center justify-center px-3 py-3 hover:text-blue transition">
-          <Link href="/">
-            <p className="text-neutralLight-neutral10 h5">Algrtm</p>
-          </Link>
-        </div>
+        <Link href="/">
+          <div className="flex w-full items-center justify-center px-3 py-3 hover:text-blue transition">
+            <p className="text-blueLight-blue50 h5">A</p>
+            <p className="text-neutralLight-neutral10 h5">lgrtm</p>
+          </div>
+        </Link>
       </div>
 
       <div>
@@ -43,13 +42,14 @@ export default function Header() {
             <button className="buttonPrimary p3">Contact Us</button>
           </Link>
           <button className="buttonSecondary">
-            <div className="flex items-center justify-center">
-              <SunIcon />
+            <div className="flex items-center justify-center gap-x-2">
+              {/* <SunIcon /> */}
+              <Image src="/sun.svg" width={18} height={18} alt="Sun icon" />
               <Image
-                src="/ellipse.svg"
+                src="/circle.svg"
                 width={24}
                 height={24}
-                alt="ellipse icon"
+                alt="Ellipse icon"
               />
             </div>
           </button>

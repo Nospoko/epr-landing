97e77c89c1d73e7a359e64a1095189e762735a56
Fermatka <agatka.fermatka@gmail.com>
2024-02-20 13:53:06 +0100

    folders structure refactor

--- algrtm/src/app/layout.js
+++ algrtm/src/app/layout.js
@@ -1,5 +1,5 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 
 const pingFont = localFont({ src: "../fonts/PingFangHK.ttf" });

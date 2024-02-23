134b85d77eec99accc314f548b1ec7f969a8594c
Fermatka <agatka.fermatka@gmail.com>
2024-02-23 22:08:37 +0100

    LinksSection basic layout added

--- algrtm/src/app/layout.js
+++ algrtm/src/app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
+import LinksSection from "@/components/LinksSection";
 
 const pingFont = localFont({ src: "../fonts/PingFangHK.ttf" });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       >
         <Navbar />
         {children}
+        <LinksSection />
         <Footer />
       </body>
     </html>
---
+++ algrtm/src/components/LinksSection.js
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import React from "react";
+
+const LinksSection = () => {
+  return (
+    <div className="flex flex-col bg-neutralLight-neutral10 mx-[12.5rem] my-16 gap-16">
+      <div className="flex gap-[11.125rem]   bg-neutralLight-neutral10 ">
+        <div className=" flex flex-col gap-6 ">
+          <div className="flex w-full justify-start h5">
+            <p className="text-blueLight-blue50 h5">A</p>
+            <p className="text-neutralLight-neutral100 h5">lgrtm</p>
+          </div>
+          <div className="p2 text-neutralLight-neutral60 max-w-[25.375rem]">
+            We develop algorithmic solutions with Machine Learning, Research and
+            Prototyping, Data Quality Engineering, Analytics Automation,
+            Creative Coding, Dataset Management.
+          </div>
+        </div>
+        {/* sekcja z linkami */}
+        <div className="flex gap-[0.625rem] px-4 items-end">
+          <Link
+            href="#"
+            className="p3 text-neutralLight-neutral90 p-[0.625rem]"
+          >
+            PianoRoll
+          </Link>
+          <Link
+            href="#"
+            className="p3 text-neutralLight-neutral90 p-[0.625rem]"
+          >
+            Custom Projects
+          </Link>
+          <Link
+            href="#"
+            className="p3 text-neutralLight-neutral90 p-[0.625rem]"
+          >
+            Blog & News
+          </Link>
+          <Link
+            href="#"
+            className="p3 text-neutralLight-neutral90 p-[0.625rem]"
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <hr className="w-full border-t border-gray-400"></hr>
+      </div>
+
+      {/* <div>
+        <Image src="/line.svg" width={65} height={0} alt="Line" />
+      </div> */}
+
+      <div className="flex gap-11 flex-center justify-center text-neutralLight-neutral90 p3">
+        <Link href="#" className="text-neutralLight-neutral90 p3">
+          Linkedin
+        </Link>
+        <Link href="#" className="text-neutralLight-neutral90 p3">
+          Github
+        </Link>
+        <Link href="#" className="text-neutralLight-neutral90 p3">
+          Clutch
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LinksSection;

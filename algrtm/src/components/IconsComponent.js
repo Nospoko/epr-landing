80d58a7bcba9645c9966434089da75d656561d7d
Fermatka <agatka.fermatka@gmail.com>
2024-02-29 09:23:29 +0100

    added hovers for logos in IconsComponent

--- algrtm/src/components/IconsComponent.js
+++ algrtm/src/components/IconsComponent.js
@@ -9,12 +9,21 @@ const IconsComponent = () => {
         width={24.1}
         height={24}
         alt="Python logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
       <Image
         src="/assets/logos/pandas-logo.svg"
         width={24}
         height={24}
         alt="Pandas logo"
+        className="transition-transform duration-300 transform hover:scale-125"
+      ></Image>
+      <Image
+        src="/assets/logos/hugging-logo.svg"
+        width={24}
+        height={24}
+        alt="Huggling face logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
 
       <Image
@@ -22,42 +31,49 @@ const IconsComponent = () => {
         width={24}
         height={24}
         alt="Numpy logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
       <Image
         src="/assets/logos/redis-logo.svg"
         width={27.93}
         height={24}
         alt="Redis logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
       <Image
         src="/assets/logos/postgreSQL-logo.svg"
         width={24}
         height={24}
         alt="PostgreSQL logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
       <Image
         src="/assets/logos/AWS-logo.svg"
         width={40.96}
         height={24}
         alt="AWS logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
       <Image
         src="/assets/logos/docker-logo.svg"
         width={33.45}
         height={24}
         alt="Docker logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
       <Image
         src="/assets/logos/pyTorch-logo.svg"
         width={24}
         height={24}
         alt="PyTorch logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
       <Image
         src="/assets/logos/streamlit-logo.svg"
         width={43.83}
         height={24}
         alt="Streamlit logo"
+        className="transition-transform duration-300 transform hover:scale-125"
       ></Image>
     </div>
   );

61d0f4763d57a8d52c055ab0102a31632617533e
Fermatka <agatka.fermatka@gmail.com>
2024-03-14 21:03:28 +0100

    PianoRoll page in progress

--- algrtm/src/app/pianoRoll/page.js
+++ algrtm/src/app/pianoRoll/page.js
@@ -1,8 +1,21 @@
 import PianoRollDescription from "@/components/PianoRollDescription";
+import RoundedTextComponent from "@/components/RoundedTextComponent";
+import SolutionsSection from "@/components/SolutionsSection";
 import React from "react";
 
 function PianoRoll() {
-  return <PianoRollDescription />;
+  return (
+    <main className="flex flex-col">
+      <PianoRollDescription />
+      <RoundedTextComponent
+        version="version3"
+        backgroundColor="neutralLight-neutral10"
+        textTitleColor="neutralLight-neutral100"
+        textParagraph1Color="neutralLight-neutral100"
+      />
+      <SolutionsSection version="version1" />
+    </main>
+  );
 }
 
 export default PianoRoll;

65d65f3c6d1881f2ac3159eeb450c353f353d7f5
Fermatka <agatka.fermatka@gmail.com>
2024-03-19 11:34:10 +0100

    basic ContactForm layout added

--- algrtm/src/app/contactUs/page.js
+++ algrtm/src/app/contactUs/page.js
@@ -1,7 +1,20 @@
+import ContactDescription from "@/components/ContactDescription";
+import ContactForm from "@/components/ContactForm";
+import SolutionsSection from "@/components/SolutionsSection";
+import RoundedRectangle from "@/components/shared/RoundedRectangle";
 import React from "react";
 
 const ContactUs = () => {
-  return <div>Contact Us</div>;
+  return (
+    <section className="flex flex-col items-start  mt-[4.94rem] ">
+      <div className="flex items-start gap-[4rem] mx-[12.5rem]">
+        <ContactDescription />
+        <ContactForm />
+      </div>
+      <RoundedRectangle />
+      <SolutionsSection version="version1" />
+    </section>
+  );
 };
 
 export default ContactUs;
---
+++ algrtm/src/components/ContactForm.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <section className="text-gray-600 body-font relative">
+      <div className="w-full">
+        <div className="">
+          <div className="flex flex-wrap -m-2">
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label
+                  htmlFor="name"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Name
+                </label>
+                <input
+                  placeholder="name"
+                  type="text"
+                  id="name"
+                  name="Name"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Your email
+                </label>
+                <input
+                  placeholder="Your email"
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <div className="relative">
+                <label
+                  htmlFor="message"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Tell us more
+                </label>
+                <textarea
+                  placeholder="Tell us more"
+                  id="message"
+                  name="message"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                ></textarea>
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <button className="flex mx-auto text-white bg-neutralLight-neutral10 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Send message
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;

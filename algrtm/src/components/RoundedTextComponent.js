53b989201c6413ba771c403bf90ee519c80356e6
Fermatka <agatka.fermatka@gmail.com>
2024-04-02 12:48:59 +0200

    basic responsiveness added to RoundedTextComponent

--- algrtm/src/components/RoundedTextComponent.js
+++ algrtm/src/components/RoundedTextComponent.js
@@ -13,23 +13,23 @@ const RoundedTextComponent = ({
 }) => {
   const { title, paragraph1, paragraph2 } = texts[version];
   return (
-    <section className="w-full px-[5.875rem]">
+    <section className="w-full px-[1rem] sm:px-[5.875rem] ">
       <div
-        className={`bg-${backgroundColor} w-full flex flex-col items-start px-[6.75rem] py-[5.88rem] rounded-[2rem]`}
+        className={`bg-${backgroundColor} w-full flex flex-col items-center sm:items-start py-[2.75rem] px-[2rem] sm:py-[5.88rem] sm:px-[6.75rem] rounded-[2rem]`}
       >
         <div className="max-w-[51.75rem]">
           <div className={`flex flex-col gap-${gap}`}>
-            <div className={`h2 leading-[4rem] text-${textTitleColor} `}>
+            <div
+              className={`h2SB leading-[2.75rem] sm:h2SB sm:leading-[3.25rem] lg:h2 text-${textTitleColor} `}
+            >
               {title}
             </div>
-            <div className={`h6 leading-[1.98rem] text-${textParagraph1Color}`}>
+            <div className={`h6_400 lg:h6 text-${textParagraph1Color}`}>
               {paragraph1}
             </div>
           </div>
 
-          <div
-            className={`p2 leading-[1.64rem] mt-4 text-${textParagraph2Color}`}
-          >
+          <div className={`p2 mt-4 text-${textParagraph2Color}`}>
             {paragraph2}
           </div>
         </div>

06619e69844d3e9c605893cb51cc95b495341d41
Fermatka <agatka.fermatka@gmail.com>
2024-04-02 08:55:07 +0200

    validateForm function refactored

--- algrtm/src/utils/validation.js
+++ algrtm/src/utils/validation.js
@@ -1,18 +1,18 @@
 const validateForm = (name, email, message) => {
   const errors = {};
 
-  if (!name) {
-    errors.your_name = "Name field cannot be empty";
+  if (!name.trim()) {
+    errors.your_name = "Name field cannot be empty.";
   }
 
   if (!email) {
-    errors.your_email = "Email field cannot be empty";
+    errors.your_email = "Email field cannot be empty.";
   } else if (!/\S+@\S+\.\S+/.test(email)) {
-    errors.your_email = "Invalid email address";
+    errors.your_email = "Invalid email address.";
   }
 
-  if (!message) {
-    errors.message = "Message field cannot be empty";
+  if (!message.trim()) {
+    errors.message = "Message field cannot be empty.";
   }
 
   return errors;

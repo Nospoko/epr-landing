495543afb96386462d855d9514882ebe60bdad82
Fermatka <agatka.fermatka@gmail.com>
2024-04-10 11:22:53 +0200

    TechIconsComponent responsiveness refactored

--- algrtm/src/components/shared/TechIconsComponent.js
+++ algrtm/src/components/shared/TechIconsComponent.js
@@ -12,10 +12,6 @@ const TechIconsComponent = ({
       className={`flex flex-wrap items-stretch w-full gap-[1.38rem] ${classNames}`}
     >
       {imagesArray.map((image) => {
-        const customSize = image[size].s_width
-          ? `w-[${image[size].s_width}px] h-[${image[size].s_height}px] md:w-[${image[size].width}px] md:h-[${image[size].height}px]`
-          : "";
-
         return (
           <Image
             key={image.name}
@@ -28,9 +24,9 @@ const TechIconsComponent = ({
             alt={`${
               image.name.charAt(0).toUpperCase() + image.name.slice(1)
             } logo`}
-            className={`transition-transform duration-300 transform hover:scale-125 ${customSize} ${
-              !color && "grayscale"
-            }`}
+            className={`transition-transform duration-300 transform hover:scale-125 ${
+              image.name
+            } ${!color && "grayscale"}`}
           />
         );
       })}

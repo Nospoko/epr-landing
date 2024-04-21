1d039347ef82c84bd246a88ff64d4da33a717829
Fermatka <agatka.fermatka@gmail.com>
2024-04-21 15:58:57 +0200

    problem with Linkedin.js solving in progress

--- algrtm/src/components/icons/GitHub.js
+++ algrtm/src/components/icons/GitHub.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Github = () => {
+const Github = ({ color, className }) => {
   return (
     <svg
       width="45"
--- algrtm/src/components/icons/LinkedIn.js
+++ algrtm/src/components/icons/LinkedIn.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Linkedin = () => {
+const Linkedin = ({ color, className }) => {
   return (
     <svg
       width="45"

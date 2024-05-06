15cb9396c12e184a7a510201177425622a7fcf73
Fermatka <agatka.fermatka@gmail.com>
2024-05-06 09:38:48 +0200

    Image added to OpportiunitiesIndustrySection

--- algrtm/src/components/OpportunitiesIndustrySection.js
+++ algrtm/src/components/OpportunitiesIndustrySection.js
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import CollapsibleSectionsComponent from "./shared/CollapsibleSectionsComponent";
+import Image from "next/image";
 
 const OpportunitiesIndustrySection = () => {
   return (
@@ -11,7 +12,17 @@ const OpportunitiesIndustrySection = () => {
       <div className="flex flex-col gap-4 w-full xl:px-[5.875rem]">
         <div className="flex flex-col md:flex-row gap-[1.5rem]  bg-neutralLight-neutral90 dark:bg-neutralDark-neutral90 px-[2.75rem] py-[2.75rem] md:py-[5.88rem]  xl:py-[5.85rem] xl:px-[6.75rem] rounded-[2rem]">
           <div className=" h5_small_bold md:h4SB lg:h4 text-neutralLight-neutral10 dark:text-neutralDark-neutral10 flex justify-start max-w-[25.1875rem] w-full">
-            <p>There'll be image here</p>
+            <Image
+              src="/factory-image.png"
+              alt="Image of the factory inside"
+              width={440}
+              height={554}
+              sizes="100vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
           </div>
 
           <CollapsibleSectionsComponent version="version3" />

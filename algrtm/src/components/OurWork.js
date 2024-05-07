14e01aac2db9153b1b50ca53ce4a98e8edbf5f64
Fermatka <agatka.fermatka@gmail.com>
2024-05-07 09:35:05 +0200

    OurWork upaded with proper styles

--- algrtm/src/components/OurWork.js
+++ algrtm/src/components/OurWork.js
@@ -4,12 +4,12 @@ import CustomIcon from "./shared/CustomIcon";
 
 const OurWork = () => {
   return (
-    <section className="mt-[2.75rem] w-full justify-center items-center text-center max-w-[78.25rem] flex flex-col gap-6 mx-[1rem] md:max-w-[54.875rem] md:mt-[7.75rem md:mx-[4.56rem] xl:m-[5.88rem] bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100">
+    <section className=" w-full flex flex-col justify-center items-center text-center gap-6 mt-[2.75rem] mx-[1rem] md:mt-[7.75rem] md:mx-[4.56rem] xl:mx-[5.88rem] bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100">
       <h4 className="h4 lg:h2 text-neutralLight-neutral10 dark:text-neutralDark-neutral10">
         Our work
       </h4>
-      <div className="flex flex-col lg:flex-row w-full gap-[1.5rem] md:flex-row xl:gap-[2rem] mt-[2rem]">
-        <div className="flex flex-col justify-end  items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] max-w-[20.25rem] text-neutralLight-neutral100 bg-[url('/heavyIndustry-bg-image.jpeg')]">
+      <div className="flex flex-col items-center justify-center w-full lg:flex-row gap-[1.5rem] md:flex-row xl:gap-[2rem] mt-[2rem]">
+        <div className=" flex flex-col justify-end items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] w-[21.44rem] text-neutralLight-neutral100 bg-[url('/heavyIndustry-bg-image.jpeg')]">
           <div className="flex flex-col shrink-0 items-start text-start justify-self-end gap-[1.5rem] px-[1.5rem] py-[2.75rem] lg:p-[2rem] xl:p-[2.75rem]">
             <h5 className="h5_small mt-[3rem]">Heavy Industry </h5>
             <p className="p2">Cement Manufacturing Data Laboratory</p>
@@ -59,7 +59,7 @@ const OurWork = () => {
           </div>
         </div>
       </div>
-      <div className="mt-[1.5rem] flex flex-col gap-[1rem] items-center self-stretch  w-full px-[1.5rem] md:px-[3.38rem] xl:mx-[6.75rem] py-[5.88rem]  text-neutralLight-neutral10 dark:text-neutralDark-neutral10 bg-neutralLight-neutral90 dark:bg-neutralDark-neutral90 rounded-[2rem]">
+      <div className="mt-[1.5rem] flex flex-col gap-[1rem] items-center w-full max-w-[64.75rem] px-[1.5rem] md:px-[3.38rem] xl:mx-[6.75rem] py-[5.88rem] text-neutralLight-neutral10 dark:text-neutralDark-neutral10 bg-neutralLight-neutral90 dark:bg-neutralDark-neutral90 rounded-[2rem]">
         <h5 className="h5_500">No chatbots</h5>
         <h6 className="h6_600">
           We love and appreciate all of our AI chatting friends, but if you’re

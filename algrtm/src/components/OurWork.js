9aa0fbf65cb03eafe4b4e2b9acac6a994409ae65
Fermatka <agatka.fermatka@gmail.com>
2024-05-13 12:34:56 +0200

    Ourwork.js modified, added images brightness to correct texts visuality

--- algrtm/src/components/OurWork.js
+++ algrtm/src/components/OurWork.js
@@ -9,9 +9,10 @@ const OurWork = () => {
         Our work
       </h4>
       <div className="flex flex-col items-center justify-center w-full lg:flex-row gap-[1.5rem] md:flex-row xl:gap-[2rem] mt-[2rem]">
-        <div className=" flex flex-col justify-end items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] text-neutralLight-neutral100 bg-[url('/heavyIndustry-bg-image.jpeg')] w-full md:w-[20.25rem] h-[25rem] bg-center bg-cover">
-          <div className="flex flex-col shrink-0 items-start text-start justify-self-end gap-[1.5rem] px-[1.5rem] py-[2.75rem] lg:p-[2rem] xl:p-[2.75rem]">
-            <h5 className="h5_small mt-[3rem]">Heavy Industry </h5>
+        <div className="relative flex flex-col justify-end items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] w-full md:w-[20.25rem] h-[25rem] bg-center bg-cover text-neutralLight-neutral100">
+          <div className="absolute inset-0 bg-[url('/heavyIndustry-bg-image.jpeg')] bg-no-repeat bg-cover bg-center brightness-50 rounded-[2rem]"></div>
+          <div className="absolute inset-0 flex flex-col justify-end items-start text-left self-stretch gap-[1.5rem] px-[1.5rem] py-[2.75rem] lg:p-[2rem] xl:p-[2.75rem]">
+            <h5 className="h5_small mt-[3rem] test-left">Heavy Industry </h5>
             <p className="p2">Cement Manufacturing Data Laboratory</p>
             <div className="flex items-start">
               <CustomLink
@@ -19,14 +20,16 @@ const OurWork = () => {
                 text="Learn more"
                 customIcon={<CustomIcon name="ArrowUpRight" color="white" />}
                 alt="arrow up icon"
-                href="/heavyIndustry"
+                href="/medTech"
               />
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-end  items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] bg-[url('/labAssistant-bg-image.jpeg')] w-full md:w-[20.25rem] h-[25rem] bg-center bg-cover text-neutralLight-neutral100">
-          <div className="flex flex-col shrink-0 items-start text-start justify-self-end gap-[1.5rem] px-[1.5rem] py-[2.75rem] lg:p-[2rem] xl:p-[2.75rem]">
-            <h5 className="h5_small mt-[3rem]">Med Tech </h5>
+
+        <div className="relative flex flex-col justify-end items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] w-full md:w-[20.25rem] h-[25rem] bg-center bg-cover text-neutralLight-neutral100">
+          <div className="absolute inset-0 bg-[url('/labAssistant-bg-image.jpeg')] bg-no-repeat bg-cover bg-center brightness-50 rounded-[2rem]"></div>
+          <div className="absolute inset-0 flex flex-col justify-end items- text-left self-stretch gap-[1.5rem] px-[1.5rem] py-[2.75rem] lg:p-[2rem] xl:p-[2.75rem]">
+            <h5 className="h5_small mt-[3rem] test-left">Med Tech </h5>
             <p className="p2">
               ECG Signal Analysis Engine for Multi-lead Holter Exams
             </p>
@@ -41,9 +44,11 @@ const OurWork = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-end  items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] bg-[url('/pianoPlaying-bg-image.jpeg')] w-full md:w-[20.25rem] h-[25rem] bg-center bg-cover text-neutralLight-neutral100">
-          <div className="flex flex-col shrink-0 items-start text-start justify-self-end gap-[1.5rem] px-[1.5rem] py-[2.75rem] lg:p-[2rem] xl:p-[2.75rem]">
-            <h5 className="h5_small mt-[3rem]">Piano for AI</h5>
+
+        <div className="relative flex flex-col justify-end items-center self-stretch pt-[9.29169rem] pb-[0.02081rem] rounded-[2rem] w-full md:w-[20.25rem] h-[25rem] bg-center bg-cover text-neutralLight-neutral100">
+          <div className="absolute inset-0 bg-[url('/pianoPlaying-bg-image.jpeg')] bg-no-repeat bg-cover bg-center brightness-50 rounded-[2rem]"></div>
+          <div className="absolute inset-0 flex flex-col justify-end items- text-left self-stretch gap-[1.5rem] px-[1.5rem] py-[2.75rem] lg:p-[2rem] xl:p-[2.75rem]">
+            <h5 className="h5_small mt-[3rem] test-left">Piano for AI </h5>
             <p className="p2">
               We open the door to a mathematical understanding of music
             </p>
@@ -53,7 +58,7 @@ const OurWork = () => {
                 text="Learn more"
                 customIcon={<CustomIcon name="ArrowUpRight" color="white" />}
                 alt="arrow up icon"
-                href="/pianoRoll"
+                href="/medTech"
               />
             </div>
           </div>

d6e5b4e4eafa61ef6b9333af7f94dfbd6396fa2e
Fermatka <agatka.fermatka@gmail.com>
2024-05-13 10:45:29 +0200

    changed image in OpportunitiesIndustrySection, need to add proper size

--- algrtm/src/components/OpportunitiesIndustrySection.js
+++ algrtm/src/components/OpportunitiesIndustrySection.js
@@ -12,7 +12,7 @@ const OpportunitiesIndustrySection = () => {
       </h2>
       <div className="flex flex-col gap-4 w-full xl:px-[5.875rem]">
         <div className="flex flex-col md:flex-row gap-[1.5rem]  bg-neutralLight-neutral90 dark:bg-neutralDark-neutral90 px-[2.75rem] py-[2.75rem] md:py-[5.88rem]  xl:py-[5.85rem] xl:px-[6.75rem] rounded-[2rem]">
-          <div className=" h5_small_bold md:h4SB lg:h4 text-neutralLight-neutral10 dark:text-neutralDark-neutral10 flex justify-start max-w-[25.1875rem] w-full h-full">
+          <div className=" h5_small_bold md:h4SB lg:h4 text-neutralLight-neutral10 dark:text-neutralDark-neutral10 flex justify-start max-w-[25.1875rem] w-full h-full ">
             <Image
               src="/factory-image.png"
               alt="Image of the factory inside"
@@ -22,6 +22,7 @@ const OpportunitiesIndustrySection = () => {
               style={{
                 width: "100%",
                 height: "100%",
+                borderRadius: "10px",
               }}
             />
           </div>

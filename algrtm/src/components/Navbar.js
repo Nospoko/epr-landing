d1ea83393a99247a9754b2e2305e4b388a7073dc
Fermatka <agatka.fermatka@gmail.com>
2024-05-15 11:06:15 +0200

    added proper styles to improve menu options alignment

--- algrtm/src/components/Navbar.js
+++ algrtm/src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
   };
   return (
     <nav className="z-[999] relative flex w-full justify-between items-center self-stretch border-b border-neutralLight-neutral90 dark:border-neutralDark-neutral90 border-solid p-4 md:px-[2.75rem] xl:px-[5.875rem] dark:bg-neutralDark-neutral100">
-      <div className="flex justify-start p3SB text-neutralLight-neutral10 dark:text-neutralDark-neutral10 gap-5 ">
+      <div className="flex justify-start p3SB text-neutralLight-neutral10 dark:text-neutralDark-neutral10 gap-5">
         <Link
           href="/"
           className="transition-transform duration-500 ease-in-out transform hover:scale-150 hover:rotate-180"
@@ -27,10 +27,10 @@ export default function Navbar() {
       </div>
 
       <div className="flex w-full max-w-[37rem] flex-wrap items-center justify-end gap-y-1 md:w-[initial] md:flex-nowrap gap-[1.5rem]">
-        <div className="hidden md:flex items-center justify-center gap-x-2  text-neutralLight-neutral10 dark:text-neutralDark-neutral10">
+        <div className="hidden md:flex items-center justify-center gap-x-2 text-neutralLight-neutral10 dark:text-neutralDark-neutral10">
           <Link
             className="flex w-full items-center justify-center transition md:p3SB xl:p3 p-2.5 animation focus:text-blueLight-blue50 focus:border-b-2 focus:border-blueLight-blue50"
-            href="/pianoRoll "
+            href="/pianoRoll"
           >
             Piano for AI
           </Link>
@@ -51,7 +51,7 @@ export default function Navbar() {
         </div>
 
         <Link
-          className="hidden md:flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation focus:text-blueLight-blue50 dark:focus:text-blueLight-blue50 "
+          className="hidden md:flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation focus:text-blueLight-blue50 dark:focus:text-blueLight-blue50"
           href="/contactUs"
         >
           Contact us
@@ -61,7 +61,7 @@ export default function Navbar() {
         </div>
 
         <div
-          className=" flex md:hidden items-center justify-center cursor-pointer"
+          className="flex md:hidden items-center justify-center cursor-pointer"
           onClick={handleNav}
         >
           <Image
@@ -75,94 +75,66 @@ export default function Navbar() {
         <div
           className={
             menuOpen
-              ? "fixed right-0 top-0 width-[100%] h-[100vh] bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100 p-10 ease-in duration-500"
-              : "fixed right-[-100%] top-0 width-[100%] h-screen bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100 p-10 ease-in duration-500"
+              ? "fixed right-0 top-0 w-[100%] h-[100vh] bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100 p-10 ease-in duration-500 flex items-center justify-center"
+              : "fixed right-[-100%] top-0 w-[100%] h-screen bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100 p-10 ease-in duration-500"
           }
         >
-          <div className="flex">
-            <div className="flex justify-start p3SB text-neutralLight-neutral10 dark:text-neutralDark-neutral10 gap-5 ">
+          <div className="absolute top-0 left-0 right-0 flex border-b border-solid border-neutralLight-neutral90 dark:border-neutralDark-neutral90 p-4 items-center justify-between">
+            <div className="p3SB text-neutralLight-neutral10 dark:text-neutralDark-neutral10">
               <Link
                 href="/"
                 className="transition-transform duration-500 ease-in-out transform hover:scale-150 hover:rotate-180"
                 onClick={() => setMenuOpen(false)}
               >
-                <div className="flex w-full items-center justify-center transition">
-                  <p className="text-neutralLight-neutral10 dark:text-neutralDark-neutral10 h5">
-                    EPR
-                  </p>
-                </div>
+                <p className="text-neutralLight-neutral10 dark:text-neutralDark-neutral10 h5">
+                  EPR
+                </p>
               </Link>
             </div>
-            <div className="flex w-full items-center justify-end">
-              <div
-                onClick={handleNav}
-                className="cursor-pointer transition-transform duration-500 ease-in-out transform hover:scale-150"
-              >
-                <AiOutlineClose size={21} />
-              </div>
+            <div
+              onClick={handleNav}
+              className="cursor-pointer transition-transform duration-500 ease-in-out transform hover:scale-150"
+            >
+              <AiOutlineClose size={21} />
             </div>
           </div>
 
-          {/* small screens menu */}
-          <div className="flex  text-neutralLight-neutral10 dark:text-neutralDark-neutral10 p1SB">
-            {/* <div className="flex justify-start p3SB text-neutralLight-neutral10 dark:text-neutralDark-neutral10 gap-5 ">
-              <Link
-                href="/"
-                className="transition-transform duration-500 ease-in-out transform hover:scale-150 hover:rotate-180"
-                onClick={() => setMenuOpen(false)}
-              >
-                <div className="flex w-full items-center justify-center transition">
-                  <p className="text-neutralLight-neutral10 dark:text-neutralDark-neutral10 h5">
-                    EPR
-                  </p>
-                </div>
-              </Link>
-            </div> */}
-
-            <div className="flex flex-col py-4 justify-center items-center text-center gap-[1.5rem] text-neutralLight-neutral10 dark:text-neutralDark-neutral10 p1SB">
-              {/* <Link href="/">
+          <div className="flex flex-col justify-center items-center text-center gap-[1.5rem] text-neutralLight-neutral10 dark:text-neutralDark-neutral10 p1SB">
+            <Link href="/pianoRoll">
               <li
                 onClick={() => setMenuOpen(false)}
                 className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem]"
               >
-                Home
+                Piano for AI
               </li>
-            </Link> */}
+            </Link>
 
-              <Link href="/pianoRoll">
-                <li
-                  onClick={() => setMenuOpen(false)}
-                  className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem]"
-                >
-                  Piano for AI
-                </li>
-              </Link>
-
-              <Link href="/customSolutions">
-                <li
-                  onClick={() => setMenuOpen(false)}
-                  className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem]"
-                >
-                  Custom Solutions
-                </li>
-              </Link>
-              {/* TODO: add the link when we have sth there */}
-              {/* <Link href="/knowledge">
+            <Link href="/customSolutions">
               <li
                 onClick={() => setMenuOpen(false)}
-                className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold"
+                className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem]"
               >
-                Knowledge
+                Custom Solutions
               </li>
-            </Link> */}
-              <div className="flex flex-col gap-[2rem]">
-                <Link
-                  className="flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation focus:text-blueLight-blue50 dark:focus:text-blueLight-blue50 "
-                  href="/contactUs"
-                >
-                  Contact us
-                </Link>
-                <ThemeToggle />
+            </Link>
+            {/* TODO: add the link when we have sth there */}
+            {/* <Link href="/knowledge">
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold"
+            >
+              Knowledge
+            </li>
+          </Link> */}
+            <div className="flex flex-col gap-[2rem]">
+              <Link
+                className="flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation focus:text-blueLight-blue50 dark:focus:text-blueLight-blue50"
+                href="/contactUs"
+              >
+                Contact us
+              </Link>
+              <ThemeToggle />
+              <div className="mb-[1.5rem] flex justify-end items-end">
                 <SocialLinksSection />
               </div>
             </div>

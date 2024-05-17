05082dc35a17cc3a1aaa67b2a1f290dd4b8b4259
Fermatka <agatka.fermatka@gmail.com>
2024-05-17 14:12:26 +0200

    dark/light mode page blinking removed

--- algrtm/src/app/layout.js
+++ algrtm/src/app/layout.js
@@ -3,26 +3,39 @@ import "../styles/globals.css";
 import Footer from "@/components/Footer";
 import LinksSection from "@/components/LinksSection";
 import Header from "../components/Header";
-import ThemeContextProvider from "@/contexts/ThemeContext";
+// import ThemeContextProvider from "@/contexts/ThemeContext";
+import { Providers } from "@/providers/providers";
 
 const pingFont = localFont({ src: "../fonts/PingFangHK.ttf" });
 
 export const metadata = {
-  title: "Algrtm | Algorithmic solutions",
-  description: "We develop algorithmic solutions.",
+  title: "EPR | Algorithmic solutions",
+  description: "Make Data Science Great Again.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${pingFont.className} dark:bg-neutralDark-neutral100`}>
-        <ThemeContextProvider>
+        <Providers>
           <Header />
           {children}
           <LinksSection />
           <Footer />
-        </ThemeContextProvider>
+        </Providers>
       </body>
     </html>
+
+    // TODO: delete when works
+    // <html lang="en">
+    //   <body className={`${pingFont.className} dark:bg-neutralDark-neutral100`}>
+    //     <ThemeContextProvider>
+    //       <Header />
+    //       {children}
+    //       <LinksSection />
+    //       <Footer />
+    //     </ThemeContextProvider>
+    //   </body>
+    // </html>
   );
 }

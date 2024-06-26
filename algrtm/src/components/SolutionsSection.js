59d61f24da63f4a153b9213a9f8112ad552db9bd
Fermatka <agatka.fermatka@gmail.com>
2024-06-26 11:51:14 +0200

    letterSpacing updated in SolutionsSection

--- algrtm/src/components/SolutionsSection.js
+++ algrtm/src/components/SolutionsSection.js
@@ -9,7 +9,7 @@ const SolutionsSection = ({ version }) => {
   return (
     <section className="bg-neutralLight-neutral10 dark:bg-neutralDark-neutral90 w-full flex justify-center">
       <div className="pt-[5.88rem] pb-[5.88rem] md:pt-[7.75rem] md:pb-[7.75rem] flex flex-col justify-center items-center gap-11 max-w-[49.375rem]">
-        <h4 className="text-neutralLight-neutral100 h2SB xl:h2 justify-center items-center text-center">
+        <h4 className="text-neutralLight-neutral100 h2SB xl:h2 xl:leading-[4.8rem] justify-center items-center text-center">
           {title}
         </h4>
         <div className=" flex gap-4 justify-center items-center">

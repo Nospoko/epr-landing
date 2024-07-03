9db6d0f02051b1eeb14947650a3c5a197ca5ca0c
Fermatka <agatka.fermatka@gmail.com>
2024-07-03 10:22:51 +0200

    updated logic for changing color for choose option in Navbar

--- algrtm/src/components/Navbar.js
+++ algrtm/src/components/Navbar.js
@@ -8,15 +8,24 @@ import Menu from "./icons/Menu";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState("");
+
   const handleNav = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(menu);
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="z-[999] relative flex w-full justify-between items-center self-stretch border-b border-neutralLight-neutral90 dark:border-neutralDark-neutral90 border-solid p-4 md:px-[2.75rem] xl:px-[5.875rem] dark:bg-neutralDark-neutral100">
       <div className="flex justify-start p3SB text-neutralLight-neutral10 dark:text-neutralDark-neutral10 gap-5">
         <Link
           href="/"
           className="transition-transform duration-500 ease-in-out transform hover:scale-150"
+          onClick={() => handleMenuClick("/")}
         >
           <div className="flex w-full items-center justify-center transition">
             <p className="text-neutralLight-neutral10 dark:text-neutralDark-neutral10 h5">
@@ -29,31 +38,46 @@ export default function Navbar() {
       <div className="flex w-full max-w-[37rem] flex-wrap items-center justify-end gap-y-1 md:w-[initial] md:flex-nowrap gap-[1.5rem]">
         <div className="hidden md:flex items-center justify-center gap-x-2 text-neutralLight-neutral10 dark:text-neutralDark-neutral10">
           <Link
-            className="flex w-full items-center justify-center transition md:p3SB xl:p3 p-2.5 animation focus:text-blueLight-blue50 focus:border-b-2 focus:border-blueLight-blue50"
+            className={`flex w-full items-center justify-center transition md:p3SB xl:p3 p-2.5 animation ${
+              selectedMenu === "pianoRoll"
+                ? "text-blueLight-blue50 border-b-2 border-blueLight-blue50"
+                : ""
+            } hover:text-blueLight-blue50`}
             href="/pianoRoll"
+            onClick={() => handleMenuClick("pianoRoll")}
           >
             Piano for AI
           </Link>
 
           <Link
-            className="flex w-full items-center justify-center text-nowrap transition md:p3SB xl:p3 p-2.5 animation focus:text-blueLight-blue50 focus:border-b-2 focus:border-blueLight-blue50"
+            className={`flex w-full items-center justify-center text-nowrap transition md:p3SB xl:p3 p-2.5 animation ${
+              selectedMenu === "customSolutions"
+                ? "text-blueLight-blue50 border-b-2 border-blueLight-blue50"
+                : ""
+            } hover:text-blueLight-blue50`}
             href="/customSolutions"
+            onClick={() => handleMenuClick("customSolutions")}
           >
             Custom Solutions
           </Link>
           {/* TODO- uncomment when we have sth to show on Knowledge page */}
           {/* <Link
-            className="flex w-full items-center justify-center transition md:p3SB xl:p3 p-2.5 animation focus:text-blueLight-blue50 focus:border-b-2 focus:border-blueLight-blue50"
+            className={`flex w-full items-center justify-center transition md:p3SB xl:p3 p-2.5 animation ${
+              selectedMenu === "knowledge" ? "text-blueLight-blue50 border-b-2 border-blueLight-blue50" : ""
+            } hover:text-blueLight-blue50`}
             href="/knowledge"
+            onClick={() => handleMenuClick("knowledge")}
           >
             Knowledge
           </Link> */}
         </div>
 
         <Link
-          className="hidden md:flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation focus:text-blueLight-blue50 dark:focus:text-blueLight-blue50
-          hover:bg-neutralLight-neutral20"
+          className={`hidden md:flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation ${
+            selectedMenu === "contactUs" ? "text-blueLight-blue50" : ""
+          } hover:bg-neutralLight-neutral20`}
           href="/contactUs"
+          onClick={() => handleMenuClick("contactUs")}
         >
           Contact us
         </Link>
@@ -102,16 +126,24 @@ export default function Navbar() {
           <div className=" flex flex-grow flex-col justify-center items-center text-center gap-[1.5rem] text-neutralLight-neutral10 dark:text-neutralDark-neutral10 p1SB w-full">
             <Link href="/pianoRoll">
               <li
-                onClick={() => setMenuOpen(false)}
-                className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem]"
+                onClick={() => handleMenuClick("pianoRoll")}
+                className={`py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem] ${
+                  selectedMenu === "pianoRoll"
+                    ? "text-blueLight-blue50 border-b-2 border-blueLight-blue50"
+                    : ""
+                }`}
               >
                 Piano for AI
               </li>
             </Link>
             <Link href="/customSolutions">
               <li
-                onClick={() => setMenuOpen(false)}
-                className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem]"
+                onClick={() => handleMenuClick("customSolutions")}
+                className={`py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold p-[0.625rem] ${
+                  selectedMenu === "customSolutions"
+                    ? "text-blueLight-blue50 border-b-2 border-blueLight-blue50"
+                    : ""
+                }`}
               >
                 Custom Solutions
               </li>
@@ -119,18 +151,21 @@ export default function Navbar() {
             {/* TODO: add the link when we have sth there */}
             {/* <Link href="/knowledge">
               <li
-                onClick={() => setMenuOpen(false)}
-                className="py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold"
+                onClick={() => handleMenuClick("knowledge")}
+                className={`py-4 cursor-pointer list-none hover:text-blueLight-blue50 transition-transform duration-500 ease-in-out transform hover:scale-110 active:font-semibold ${
+                  selectedMenu === "knowledge" ? "text-blueLight-blue50 border-b-2 border-blueLight-blue50" : ""
+                }`}
               >
                 Knowledge
               </li>
             </Link> */}
             <div className="flex flex-col gap-[2rem]">
               <Link
-                className="flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation focus:text-blueLight-blue50 dark:focus:text-blueLight-blue50
-                hover:bg-neutralLight-neutral20"
+                className={`flex justify-center items-center select-none text-sm px-5 py-2.5 h-10 gap-4 rounded-md border border-neutralLight-neutral40 bg-neutralLight-neutral10 dark:bg-neutralDark-neutral10 text-neutralLight-neutral100 dark:text-neutralDark-neutral100 p3SB animation ${
+                  selectedMenu === "contactUs" ? "text-blueLight-blue50" : ""
+                }`}
                 href="/contactUs"
-                onClick={() => setMenuOpen(false)}
+                onClick={() => handleMenuClick("contactUs")}
               >
                 Contact us
               </Link>

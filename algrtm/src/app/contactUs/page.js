c4187be4f211a9c79eba8636be9eb65265abba30
Fermatka <agatka.fermatka@gmail.com>
2024-07-03 09:46:19 +0200

    updated styles in ContactUs page

--- algrtm/src/app/contactUs/page.js
+++ algrtm/src/app/contactUs/page.js
@@ -7,7 +7,7 @@ import React from "react";
 const ContactUs = () => {
   return (
     <section className="flex flex-col items-start mt-[2.75rem] md:mt-[4rem] xl:mt-[4.94rem] w-full">
-      <div className="md:flex flex-row flex-column items-center md:items-start gap-[2.75rem] md:gap-[4rem] px-[1rem] md:px-[2.75rem] xl:px-[12.5rem] w-full">
+      <div className="md:flex flex-row flex-column items-center md:items-start gap-[2.75rem] md:gap-[4rem] px-[1rem] md:px-[2.75rem] xl:px-[12.5rem] w-full pb-[6.68rem]">
         <ContactDescription />
         <ContactFormContainer />
       </div>

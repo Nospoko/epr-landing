3f682274c1182b564aba49af0d6554e3b6fdca50
Fermatka <agatka.fermatka@gmail.com>
2024-07-31 09:34:59 +0200

    added basic responiseveness

--- algrtm/src/components/PianoRollLogoDescription.js
+++ algrtm/src/components/PianoRollLogoDescription.js
@@ -3,16 +3,16 @@ import React from "react";
 
 const PianoRollLogoDescription = () => {
   return (
-    <div className="flex max-w-[64.75rem] items-center gap-[4.5rem] my-[7.19rem]">
-      <div className="bg-neutralLight-neutral90 rounded-[2rem] w-[36.25rem] h-[29.875rem] justify-center items-center gap-[0.625rem] shrink-0 flex">
+    <div className="flex flex-col md:flex-row max-w-[64.75rem] items-center gap-[2rem] md:gap-[4.5rem] my-[7.19rem] px-4">
+      <div className="bg-neutralLight-neutral90 rounded-[2rem] w-full md:w-[36.25rem] h-[29.875rem] flex flex-col md:flex-row justify-center items-center gap-[0.625rem]">
         <Image width={86.965} height={94} src="/pianoRollLogo.svg" />
         <Image width={218.057} height={38.198} src="/PianoRoll.svg" />
       </div>
-      <div className="flex flex-col gap-[0.625rem] shrink-0 items-start max-w-[24rem]">
+      <div className="flex flex-col gap-[0.625rem] items-start w-full md:w-auto max-w-[24rem]">
         <p className="h5_500 text-neutralLight-neutral10">
           Audio, images and text already benefit from well-established deep
           learning architectures and processing pipelines that consistently
-          yield amazing results.{" "}
+          yield amazing results.
         </p>
         <p className="text-neutralLight-neutral10">
           Recording piano performances in MIDI format introduces a new and
--- algrtm/src/components/PythonToolsSection.js
+++ algrtm/src/components/PythonToolsSection.js
@@ -4,7 +4,7 @@ import pythonToolsData from "@/data/pythonToolsData";
 
 const PythonToolsSection = () => {
   return (
-    <section className="w-full flex flex-col gap-[4.5rem] justify-center items-center text-center mt-[2.75rem] px-[5.88rem] mx-[1rem] md:mt-[7.75rem] md:mx-[4.56rem] xl:mx-[5.88rem] bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100">
+    <section className="w-full flex flex-col gap-[4.5rem] justify-center items-center text-center mt-[2.75rem] px-[1rem] md:px-[4.56rem] xl:px-[5.88rem] bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100">
       <h4 className="h4 lg:h2 text-neutralLight-neutral10 dark:text-neutralDark-neutral10 self-start md:self-center">
         Python Tools for MIDI Processing
       </h4>
@@ -12,7 +12,7 @@ const PythonToolsSection = () => {
         {Object.values(pythonToolsData).map((tool, index) => (
           <div
             key={index}
-            className="flex flex-col justify-start items-center w-full sm:w-[calc(50%-1.5rem)] md:w-[calc(33.333%-2rem)] h-[35rem] bg-neutralLight-neutral90 dark:bg-neutralDark-neutral90 rounded-[2rem]"
+            className="flex flex-col justify-start items-center w-full sm:w-full md:w-[calc(50%-1rem)] lg:w-[calc(33.333%-2rem)] h-[35rem] bg-neutralLight-neutral90 dark:bg-neutralDark-neutral90 rounded-[2rem] overflow-hidden"
           >
             <img
               src={tool.image}

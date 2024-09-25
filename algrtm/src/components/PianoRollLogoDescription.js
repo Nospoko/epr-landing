b7a2ac7a77bb00204eaee8cbe2cd815089f8c8a9
Fermatka <agatka.fermatka@gmail.com>
2024-09-25 10:40:59 +0200

    Updated PianoRollDescription images sizes

--- algrtm/src/components/PianoRollLogoDescription.js
+++ algrtm/src/components/PianoRollLogoDescription.js
@@ -4,18 +4,20 @@ import React from "react";
 const PianoRollLogoDescription = () => {
   return (
     <div className="lg:flex-row flex flex-col max-w-[64.75rem] items-center gap-[2.75rem] lg:gap-[4.5rem] my-[4rem] md:my-[7.19rem">
-      <div className="bg-neutralLight-neutral90 rounded-[2rem] w-full md:w-[36.25rem] h-[29.875rem] flex flex-col md:flex-row justify-center items-center gap-[0.625rem]">
+      <div className="bg-neutralLight-neutral90 rounded-[2rem] w-full md:w-[36.25rem] md:h-[29.875rem] h-[17.65rem] flex justify-center items-center gap-[0.625rem] px-[3.21rem] py-[6.67rem]">
         <Image
-          width={86.965}
-          height={94}
           src="/pianoRollLogo.svg"
           alt="Pianoroll logo"
+          width={86.97}
+          height={94}
+          className="w-[64.85px] h-[70.44px] md:w-[86.97px] md:h-[94px]"
         />
         <Image
-          width={218.057}
-          height={38.198}
           src="/PianoRoll.svg"
           alt="Pianoroll logo text"
+          width={218.06}
+          height={38.2}
+          className="w-[162.59px] h-[28.62px] md:w-[218.06px] md:h-[38.2px]"
         />
       </div>
       <div className="flex flex-col gap-[0.625rem] items-start w-full md:w-auto max-w-[24rem]">

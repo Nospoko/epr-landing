a43afe3543686bd2e9fd85cf26fbf206a334d592
Tomasz Roszczynialski <roszcz@gmail.com>
2024-10-14 14:48:29 +0200

    revert hero update

--- algrtm/src/components/DevelopSection.js
+++ algrtm/src/components/DevelopSection.js
@@ -5,9 +5,9 @@ const DevelopSection = () => {
   return (
     <section className="flex flex-col gap-6 justify-center items-center text-center max-w-[21.4375rem] mt-[2.75rem] mx-[1rem] md:max-w-[54.875rem] md:mt-[7.75rem md:mx-[4.56rem] xl-mx-[17.56rem] bg-neutralLight-neutral100 dark:bg-neutralDark-neutral100">
       <div className="h2 md:h1 text-neutralLight-neutral10 dark:text-neutralDark-neutral10 leading-[4.48rem] md:leading-[6.02rem] tracking-[-0.16rem] md:tracking-[-0.215rem]">
-        data research
+        data teams
         <br/>
-        data monitoring
+        data science
         <br/>
         data products
       </div>
